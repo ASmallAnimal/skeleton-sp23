341cf2890aea04a74e2bab145f6ad14b76466582
suis <suisbiglove@outlook.com>
2023-10-05 09:29:32 +0800

    finish chooser

--- proj0/src/aoa/choosers/RandomChooser.java
+++ proj0/src/aoa/choosers/RandomChooser.java
@@ -3,30 +3,56 @@ package aoa.choosers;
 import edu.princeton.cs.algs4.StdRandom;
 import aoa.utils.FileUtils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class RandomChooser implements Chooser {
     private final String chosenWord;
     private String pattern;
 
     public RandomChooser(int wordLength, String dictionaryFile) {
         // TODO: Fill in/change this constructor.
-        chosenWord = "";
+        if(wordLength < 1){
+            throw new IllegalArgumentException("word length should bigger than 1!");
+        }
+        List<String> words =  FileUtils.readWordsOfLength(dictionaryFile, wordLength);
+        if(words.isEmpty()){
+            throw new IllegalStateException("no word match the length!");
+        }
+        int numWords = words.size();
+        int randomlyChosenWordNumber = StdRandom.uniform(numWords);
+        chosenWord = words.get(randomlyChosenWordNumber);
+
+        char[] charArray = new char[wordLength];
+        Arrays.fill(charArray, '-');
+        pattern = new String(charArray, 0, wordLength);
     }
 
     @Override
     public int makeGuess(char letter) {
         // TODO: Fill in this method.
-        return 0;
+        StringBuilder newPattern = new StringBuilder(pattern);
+        int occurrences = 0;
+        for(int i = 0; i < chosenWord.length(); i++){
+            if(letter == chosenWord.charAt(i)){
+                occurrences += 1;
+                newPattern.setCharAt(i, letter);
+            }
+        }
+        pattern = newPattern.toString();
+        return occurrences;
     }
 
     @Override
     public String getPattern() {
         // TODO: Fill in this method.
-        return "";
+        return pattern;
     }
 
     @Override
     public String getWord() {
         // TODO: Fill in this method.
-        return "";
+        return chosenWord;
     }
 }
